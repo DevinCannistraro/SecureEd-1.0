e6bd30ba4dfc0a893572a997ada0ed5a35ce559c
DevinCannistraro <38543372+DevinCannistraro@users.noreply.github.com>
2022-05-01 14:09:12 -0400

    m

--- app/src/coursesearchlogic.php
+++ app/src/coursesearchlogic.php
@@ -1,4 +1,47 @@
 <?php
+
+function verify_input($inputVal){
+	if(strlen($inputVal) == 0){
+		return true;
+	}
+	for ($x = 0; $x < strlen($inputVal); $x++) {
+		$passed = false;
+		if(chr(47) < $inputVal[$x] and $inputVal[$x] < chr(58)){ // checks if number
+			$passed = true;
+		}
+		if(chr(64) < $inputVal[$x] and $inputVal[$x] < chr(91)){ // checks if lower case
+			$passed = true;
+		}
+		if(chr(96) < $inputVal[$x] and $inputVal[$x] < chr(123)){ // checks if lower case
+			$passed = true;
+		}
+		
+		if($passed == false) // if there is not a passing check fail it
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+function verify_course($inputVal){
+	if(strlen($inputVal) == 0){
+		return true;
+	}
+	
+	for ($x = 0; $x < strlen($inputVal); $x++) {
+		$passed = false;
+		if(chr(47) < $inputVal[$x] and $inputVal[$x] < chr(58)){ // checks if number
+			$passed = true;
+		}
+		if($passed == false) // if there is not a passing check fail it
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
 try {
     /*Get DB connection*/
     require_once "../src/DBController.php";
@@ -9,40 +52,50 @@ try {
     $semester = $_POST['semester'];
     $department = $_POST['department'];
 
-    //set default values if blank
-    if($courseid=="")
-    {
-        $courseid="defaultvalue!";
-    }
-    if($coursename=="")
-    {
-        $coursename="defaultvalue!";
-    }
-    if($semester=="")
-    {
-        $semester="defaultvalue!";
-    }
-    if($department=="")
-    {
-        $department="defaultvalue!";
-    }
 
-    $query = "	SELECT Section.CRN, Course.CourseName, Section.Year, Section.Semester, User.Email, Section.Location
-            FROM Section
-            CROSS JOIN Course ON Section.Course = Course.Code
-            INNER JOIN User ON Section.Instructor = User.UserID
-            WHERE (CRN LIKE '$courseid' OR '$courseid'='defaultvalue!') AND
-                    (Semester LIKE '$semester' OR '$semester'='defaultvalue!') AND
-                    (Course LIKE '$department' OR '$department'='defaultvalue!') AND
-                    (CourseName LIKE '$coursename' OR '$coursename' = 'defaultvalue!')";
+	$course_valid = verify_course($courseid);
+	$coursename_valid = verify_input($coursename);
+	$semester_valid = verify_input($semester);
+	$department_valid = verify_input($department);
 
-    $results = $db->query($query);
 
-    while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
-        $jsonArray[] = $row;
-    }
+	if($course_valid == true and $coursename_valid == true and $semester_valid == true and $department_valid == true){ // if all valid
+		//set default values if blank
+		if($courseid=="")
+		{
+			$courseid="defaultvalue!";
+		}
+		if($coursename=="")
+		{
+			$coursename="defaultvalue!";
+		}
+		if($semester=="")
+		{
+			$semester="defaultvalue!";
+		}
+		if($department=="")
+		{
+			$department="defaultvalue!";
+		}
+		$query = "	SELECT Section.CRN, Course.CourseName, Section.Year, Section.Semester, User.Email, Section.Location
+				FROM Section
+				CROSS JOIN Course ON Section.Course = Course.Code
+				INNER JOIN User ON Section.Instructor = User.UserID
+				WHERE (CRN LIKE '$courseid' OR '$courseid'='defaultvalue!') AND
+						(Semester LIKE '$semester' OR '$semester'='defaultvalue!') AND
+						(Course LIKE '$department' OR '$department'='defaultvalue!') AND
+						(CourseName LIKE '$coursename' OR '$coursename' = 'defaultvalue!')";
 
-    echo json_encode($jsonArray);
+		$results = $db->query($query);
+
+		while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+			$jsonArray[] = $row;
+		}
+
+		echo json_encode($jsonArray);
+	}else{ // if not valid
+		
+	}
 //note: since no changes happen to the database, it is not backed up on this page
 }
 
--- app/src/login.php
+++ app/src/login.php
@@ -1,4 +1,42 @@
 <?php
+
+function write_to_console($data) {
+ $console = $data;
+ if (is_array($console))
+ $console = implode(',', $console);
+
+ echo "<script>console.log('Console: " . $console . "' );</script>";
+}
+
+function verify_input($inputVal){
+	$allowedCharacters = "@."; // list of allowed characters that are not numbers or alphabet characters
+	for ($x = 0; $x < strlen($inputVal); $x++) {
+		$passed = false;
+		if(chr(47) < $inputVal[$x] and $inputVal[$x] < chr(58)){ // checks if number
+			$passed = true;
+		}
+		if(chr(64) < $inputVal[$x] and $inputVal[$x] < chr(91)){ // checks if lower case
+			$passed = true;
+		}
+		if(chr(96) < $inputVal[$x] and $inputVal[$x] < chr(123)){ // checks if lower case
+			$passed = true;
+		}
+		
+		for($y = 0; $y < strlen($allowedCharacters); $y++)
+		{
+			if($inputVal[$x] == $allowedCharacters[$y])
+			{
+				$passed = true;
+			}
+		}
+		if($passed == false) // if there is not a passing check fail it
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
 try {
     /*Get DB connection*/
     require_once "../src/DBController.php";
@@ -6,7 +44,9 @@ try {
     /*Get information from the post request*/
     $myusername = $_POST['username'];
     $mypassword = $_POST['password'];
-
+	//write_to_console($myusername[0]);
+	print($myusername[0]);
+	//write_to_console("Swag");
     //convert password to 80 byte hash using ripemd256 before comparing
     $hashpassword = hash('ripemd256', $mypassword);
 
@@ -17,15 +57,24 @@ try {
     $myusername = strtolower($myusername); //makes username noncase-sensitive
     global $acctype;
 
+	// new input check code
+	$isvalid = verify_input($myusername);
+	$results = false; // makes it fail if not valid
+	if($isvalid == true) // only run query if valid
+	{
+		//query for count
+		$query = "SELECT COUNT(*) as count FROM User WHERE Email='$myusername' AND (Password='$mypassword' OR Password='$hashpassword')";
+		$count = $db->querySingle($query);
 
-    //query for count
-    $query = "SELECT COUNT(*) as count FROM User WHERE Email='$myusername' AND (Password='$mypassword' OR Password='$hashpassword')";
-    $count = $db->querySingle($query);
+		//query for the row(s)
+		$query = "SELECT * FROM User WHERE Email='$myusername' AND (Password='$mypassword' OR Password='$hashpassword')";
+		$results = $db->query($query);
+	}
 
-    //query for the row(s)
-    $query = "SELECT * FROM User WHERE Email='$myusername' AND (Password='$mypassword' OR Password='$hashpassword')";
-    $results = $db->query($query);
-
+    
+	
+	
+	
     if ($results !== false) //query failed check
     {
         if (($userinfo = $results->fetchArray()) !== (null || false)) //checks if rows exist
