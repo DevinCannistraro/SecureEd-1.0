eb78b332feec8c1c0f7c90375773c64fee797782
JohnEgan <jmegan1210@yahoo.com>
2022-05-03 19:06:52 -0400

    Update EditAccountUpdateLogic.php
    checks user input when editing account info

--- app/src/EditAccountUpdateLogic.php
+++ app/src/EditAccountUpdateLogic.php
@@ -1,4 +1,61 @@
 <?php
+
+
+function verify_name_input($inputVal){
+	$allowedCharacters = "`-"; // list of allowed characters that are not numbers or alphabet characters
+	for ($x = 0; $x < strlen($inputVal); $x++) {
+		$passed = false;
+		
+		if(chr(64) < $inputVal[$x] and $inputVal[$x] < chr(91)){ // checks if upper case
+			$passed = true;
+		}
+		if(chr(96) < $inputVal[$x] and $inputVal[$x] < chr(123)){ // checks if lower case
+			$passed = true;
+		}
+		
+		for($y = 0; $y < strlen($allowedCharacters); $y++)
+		{
+			if($inputVal[$x] == $allowedCharacters[$y])
+			{
+				$passed = true;
+			}
+		}
+		if($passed == false) // if there is not a passing check fail it
+		{
+			return false;
+		}
+	}
+	return true;
+}
+function verify_email_input($inputVal){
+	$allowedCharacters = "@."; // list of allowed characters that are not numbers or alphabet characters
+	for ($x = 0; $x < strlen($inputVal); $x++) {
+		$passed = false;
+		if(chr(47) < $inputVal[$x] and $inputVal[$x] < chr(58)){ // checks if number
+			$passed = true;
+		}
+		if(chr(64) < $inputVal[$x] and $inputVal[$x] < chr(91)){ // checks if upper case
+			$passed = true;
+		}
+		if(chr(96) < $inputVal[$x] and $inputVal[$x] < chr(123)){ // checks if lower case
+			$passed = true;
+		}
+		
+		for($y = 0; $y < strlen($allowedCharacters); $y++)
+		{
+			if($inputVal[$x] == $allowedCharacters[$y])
+			{
+				$passed = true;
+			}
+		}
+		if($passed == false) // if there is not a passing check fail it
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
 try {
     /*Get DB connection*/
     require_once "../src/DBController.php";
@@ -17,6 +74,7 @@ try {
     $prevemail = $_POST['prevemail']; //required to find the user being updated
 
 
+
     if($acctype==null)
     {throw new Exception("input did not exist");}
 
@@ -29,26 +87,32 @@ try {
 
 
     /*Update the database with the new info*/
-    $query = "UPDATE User 
+
+    $results = false;
+    if(verify_name_input($fname && verify_name_input($lname) && verify_email_input($email))){
+        $query = "UPDATE User 
             SET Email = :email, AccType = :acctype, Password = :password, FName = :fname, LName = :lname, DOB = :dob, Year = :studentyear, Rank = :facultyrank, SQuestion = :squestion, SAnswer = :sanswer 
             WHERE Email = :prevemail";
-    $stmt = $db->prepare($query); //prevents SQL injection by escaping SQLite characters
-    $stmt->bindParam(':email', $email, SQLITE3_TEXT);
-    $stmt->bindParam(':acctype', $acctype, SQLITE3_INTEGER);
-    $stmt->bindParam(':password', $password, SQLITE3_TEXT);
-    $stmt->bindParam(':fname', $fname, SQLITE3_TEXT);
-    $stmt->bindParam(':lname', $lname, SQLITE3_TEXT);
-    $stmt->bindParam(':dob', $dob, SQLITE3_TEXT);
-    $stmt->bindParam(':studentyear', $studentyear, SQLITE3_INTEGER);
-    $stmt->bindParam(':facultyrank', $facultyrank, SQLITE3_TEXT);
-    $stmt->bindParam(':squestion', $squestion, SQLITE3_TEXT);
-    $stmt->bindParam(':sanswer', $sanswer, SQLITE3_TEXT);
-    $stmt->bindParam(':prevemail', $prevemail, SQLITE3_TEXT);
-    $results = $stmt->execute();
+        $stmt = $db->prepare($query); //prevents SQL injection by escaping SQLite characters
+        $stmt->bindParam(':email', $email, SQLITE3_TEXT);
+        $stmt->bindParam(':acctype', $acctype, SQLITE3_INTEGER);
+        $stmt->bindParam(':password', $password, SQLITE3_TEXT);
+        $stmt->bindParam(':fname', $fname, SQLITE3_TEXT);
+        $stmt->bindParam(':lname', $lname, SQLITE3_TEXT);
+        $stmt->bindParam(':dob', $dob, SQLITE3_TEXT);
+        $stmt->bindParam(':studentyear', $studentyear, SQLITE3_INTEGER);
+        $stmt->bindParam(':facultyrank', $facultyrank, SQLITE3_TEXT);
+        $stmt->bindParam(':squestion', $squestion, SQLITE3_TEXT);
+        $stmt->bindParam(':sanswer', $sanswer, SQLITE3_TEXT);
+        $stmt->bindParam(':prevemail', $prevemail, SQLITE3_TEXT);
+        $results = $stmt->execute();
+    }
+
+    
 
 //is true on success and false on failure
     if (!$results) {
-        throw new Exception("edit failed");
+        header("Location: ../public/user_search.php");
     } else {
         //backup database
         $db->backup($db, "temp", $GLOBALS['dbPath']);
